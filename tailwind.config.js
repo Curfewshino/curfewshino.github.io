f00696a3fe360ddf04349801f55300e92e3a594f
Phuchay "Mayo" M <phuchay1391@gmail.com>
2024-03-04 23:05:13 +0700

    add stuff #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}', './books/*.{html,js}'
-              ,'./books/name/*.{html,js}'
+  content: ['./*.{html,js}', './books/*.{html,js}',
+              './books/name/*.{html,js}',
+              './*.{html,js}'
             ],
   theme: {
     extend: {},
