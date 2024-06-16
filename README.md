3a45ea71d6e68a790e08bea82efeff3708d34564
Phuchay "Mayo" M. <phuchay1391@gmail.com>
2024-06-16 14:33:46 +0700

    Rewrite using SvelteKit

--- README.md
+++
@@ -1,3 +0,0 @@
-# curfewshino.github.io
-Website WIP not finished yet not even close 
-[🌐 Github Page](https://curfewshino.github.io/index)
--- menu.js
+++
@@ -1,7 +0,0 @@
-const dropdownButton = document.querySelector("#menu");
-const dropdownList = document.querySelector("#nav.hidden");
-
-
-dropdownButton.addEventListener("click", () => {
-  dropdownList.classList.toggle("hidden");
-});
